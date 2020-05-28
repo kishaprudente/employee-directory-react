b9e1a9d3343d65753d62d11ab5fa91577d1af8aa
Kisha Prudente <kisha.prudente@gmail.com>
2020-05-27 21:25:31 -0400

    removed sub header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
+import Table from "./components/Table";
 
 function App() {
 	return (
 		<div>
 			<Header />
 			<SearchBar />
+			<Table />
 		</div>
 	);
 }
