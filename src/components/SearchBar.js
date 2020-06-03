331e9869a0cb07dfb9f8ea53884d5bf416b9b39b
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-03 15:39:17 -0400

    added employees data

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,92 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import Table from "./Table";
-import faker from "faker";
+import employees from "../employees";
 
-const employees = [
-	{
-		id: 1,
-		image: faker.image.avatar(),
-		name: faker.name.findName(),
-		phone: faker.phone.phoneNumberFormat(),
-		email: "fake@testo.com",
-		dob: "09/23/93",
-	},
-	{
-		id: 2,
-		image: faker.image.avatar(),
-		name: faker.name.findName(),
-		phone: faker.phone.phoneNumberFormat(),
-		email: faker.internet.email(),
-		dob: "02/23/92",
-	},
-	{
-		id: 3,
-		image: faker.image.avatar(),
-		name: faker.name.findName(),
-		phone: faker.phone.phoneNumberFormat(),
-		email: faker.internet.email(),
-		dob: "08/13/01",
-	},
-	{
-		id: 4,
-		image: faker.image.avatar(),
-		name: faker.name.findName(),
-		phone: faker.phone.phoneNumberFormat(),
-		email: faker.internet.email(),
-		dob: "11/02/98",
-	},
-	{
-		id: 5,
-		image: faker.image.avatar(),
-		name: faker.name.findName(),
-		phone: faker.phone.phoneNumberFormat(),
-		email: faker.internet.email(),
-		dob: "01/28/83",
-	},
-	{
-		id: 6,
-		image: faker.image.avatar(),
-		name: faker.name.findName(),
-		phone: faker.phone.phoneNumberFormat(),
-		email: faker.internet.email(),
-		dob: "09/26/91",
-	},
-	{
-		id: 7,
-		image: faker.image.avatar(),
-		name: faker.name.findName(),
-		phone: faker.phone.phoneNumberFormat(),
-		email: faker.internet.email(),
-		dob: "06/11/89",
-	},
-	{
-		id: 8,
-		image: faker.image.avatar(),
-		name: faker.name.findName(),
-		phone: faker.phone.phoneNumberFormat(),
-		email: faker.internet.email(),
-		dob: "05/17/99",
-	},
-];
+class SearchBar extends Component {
+	state = {
+		employees: employees,
+		search: "",
+	};
 
-export default () => {
-	return (
-		<div>
-			<nav style={styles.navbar} className="navbar navbar-light bg-light">
-				<form className="form-inline">
-					<input
-						className="form-control mr-sm-2"
-						type="search"
-						placeholder="Search employees"
-						aria-label="Search"
-						onChange={(e) => console.log(e.target.value)}
-					/>
-				</form>
-			</nav>
-			<Table employees={employees} />
-		</div>
-	);
-};
+	componentDidMount() {
+		this.setState({ employees: employees });
+	}
+
+	handleInputChange = (event) => {
+		const { value } = event.target;
+		this.setState({
+			search: value,
+		});
+	};
+
+	searchEmployees = (query) => {
+		const filteredEmployees = this.state.employees.filter(
+			(employee) =>
+				employee.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+		);
+		return filteredEmployees;
+	};
+
+	render() {
+		return (
+			<div>
+				<nav style={styles.navbar} className="navbar navbar-light bg-light">
+					<form className="form-inline">
+						<input
+							className="form-control mr-sm-2"
+							type="search"
+							placeholder="Search employees"
+							aria-label="Search"
+							value={this.state.search}
+							onChange={this.handleInputChange}
+						/>
+					</form>
+				</nav>
+				<Table employees={this.searchEmployees(this.state.search)} />
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
 
 const styles = {
 	navbar: {
